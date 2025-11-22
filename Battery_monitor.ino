91b84237b9cbcaa82832881f4dca3738c9c5821e
Hoangquan166 <mr.hoangquanngu@gmail.com>
2025-11-23 00:12:40 +0700

    add fullhd.ino

--- Battery_monitor.ino
+++ Battery_monitor.ino
@@ -1,5 +1,7 @@
 #include <DallasTemperature.h> 
-#include <OneWire.h> 
+#include <Onewire.h> 
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include <Wire.h> 
 #include <Adafruit_INA219.h>
 
@@ -14,12 +16,24 @@ Adafruit_INA219 ina219;
   float loadvoltage = 0;
   float current_mA = 0;
   float batteryvoltage = 0;
+  float status=0;
+
+const char* ssid = "abc";
+const char* password = "12345678";
+
+const char* mqtt_server = "thingsboard.cloud";
+const int mqtt_port = 1883;
+const char* access_token = "dgvO5G7nCBvoIemOfEve";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
   sensors.begin();
   ina219.begin();
-  Wire.begin(21,22); // SDA, SCL
+  Wire.begin(21,22);
   
   // Cấu hình 12-bit, 128 samples, bus range 32V, PGA ÷8, continuous mode
   uint16_t config = 0;
@@ -35,12 +49,33 @@ void setup() {
   Wire.endTransmission();
   delay(70); 
 
-  // Vgs = 3.3V
+    // Vgs = 3.3V
   pinMode(19, OUTPUT);
   digitalWrite(19, HIGH);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+}
+ Serial.println("\nConnected to WiFi!");
+
+  client.setServer(mqtt_server, mqtt_port);
+  while (!client.connected()) {
+    Serial.print("Connecting to ThingsBoard...");
+    if (client.connect("ESP32_Device", access_token, NULL)) {
+      Serial.println("connected!");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
     //INA219
   shuntvoltage = ina219.getShuntVoltage_mV();
   loadvoltage = ina219.getBusVoltage_V();
@@ -50,15 +85,21 @@ void loop() {
     Serial.printf("Battery Voltage: %.2f V\n", batteryvoltage);
     Serial.printf("LOW BATTERY - SAFETY CUTOFF ACTIVATED\n");
     digitalWrite(19, LOW);
+    status=1;
   } else {
     Serial.printf("Battery Voltage: %.2f V\n", batteryvoltage);
+    status=0;
+    digitalWrite(19, HIGH);
   } 
   if (current_mA > 1000) {
     Serial.printf("Current: %.1f mA\n", current_mA);
     Serial.printf("OVERCURRENT - SAFETY CUTOFF ACTIVATED\n");
     digitalWrite(19, LOW);
+    status=1;
   } else {
     Serial.printf("Current: %.1f mA\n", current_mA);
+    status=0;
+    digitalWrite(19, HIGH);
   }
 /* Serial.printf("shuntvolt: %.1f mV\n", shuntvoltage);
   Serial.printf("loadvolt: %.2f V\n", loadvoltage); */
@@ -69,12 +110,34 @@ void loop() {
   tempC = sensors.getTempCByIndex(0); // lấy data từ con cảm biến đầu tiên
 if (tempC == DEVICE_DISCONNECTED_C) {
   Serial.printf("Sensor not found or CRC error!\n");
-} else if (tempC >= 60) {
+} else if (tempC >= 30) {
   Serial.printf("Temperature: %.1f°C\n", tempC);
   Serial.printf("TEMPERATURE CRITICAL, SAFETY CUTOFF ACTIVATED\n"); 
   digitalWrite(19, LOW);
+  status=1;
 } else {
   Serial.printf("Temperature: %.1f°C\n", tempC);
+  status=0;
+  digitalWrite(19, HIGH);
 }
+//Web
   delay(1000); 
+  if (!client.connected()) {
+    while (!client.connected()) {
+      Serial.println("Reconnecting to ThingsBoard...");
+      if (client.connect("ESP32_Device", access_token, NULL)) {
+        Serial.println("Reconnected!");
+      } else {
+        delay(2000);
+      }
+    }
+  }
+
+  String payload = "{\"Temperature(℃)\":" + String(tempC) + ",\"Voltage(V)\":" + String(batteryvoltage) + ",\"Current(mA)\":" + String(current_mA) + ",\"Status\":" + String(status) + "}";
+  Serial.println("Publishing data: " + payload);
+
+  client.publish("v1/devices/me/telemetry", payload.c_str());
+  client.loop();
+
+  delay(1000);
 }
